35922aa087c51ed3cee36628fa3d0e981031dec7
Mai Mohammed <maymohammedabuabed@gmail.com>
2022-08-12 13:36:26 +0300

    handling user choice

--- public/dom.js
+++ public/dom.js
@@ -7,11 +7,10 @@ fetch("/breeds" ,(response)=>{
 // --------------- handle matches to user input -----------------------------
 const searchInput = document.querySelector('.search');
 const autoComContainer = document.querySelector('.autocom-box');
-
-
+let autoComchoices =document.querySelectorAll(".autocom-box li"); 
 searchInput.addEventListener('keyup', (e) => {
   const value = e.target.value;
-
+  
   if (value !== "") {
     const matches = breeds.filter((ele) => {
       return ele.startsWith(value.toLowerCase());
@@ -20,7 +19,17 @@ searchInput.addEventListener('keyup', (e) => {
       return `<li>${ele}</li>`;
     });
     autoComContainer.innerHTML = autoComResults.join('') ;
+    autoComchoices = document.querySelectorAll(".autocom-box li");
+    // console.log(autoComchoices);
   }else{
     autoComContainer.innerHTML = "" ;
   }
+  autoComchoices.forEach((li)=>{
+    li.addEventListener("click" ,(e)=>{
+      searchInput.value = e.target.textContent;
+      autoComContainer.innerHTML="";
+    })
+  });
 });
+
+// --------------- handling user choice (put it in search input value)  -----------
