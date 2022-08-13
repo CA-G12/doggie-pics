ad5013dbd087c86800e6323aa8bc9336d45be0a1
Mai Mohammed <maymohammedabuabed@gmail.com>
2022-08-13 15:13:18 +0300

    ef

--- public/fetch.js
+++ public/fetch.js
@@ -1,12 +1,12 @@
 const fetch = (url, cb) => {
-    let xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4) {
-            if (xhr.status == 200) {
-                cb(JSON.parse(xhr.responseText));
-            }
-        }
-    };
-    xhr.open('GET', url);
-    xhr.send();
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        cb(JSON.parse(xhr.responseText));
+      }
+    }
+  };
+  xhr.open('GET', url);
+  xhr.send();
 };
