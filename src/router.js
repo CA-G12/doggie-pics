de20d9fe4493b71994775b591e144fd76a31b11b
Mai Mohammed <maymohammedabuabed@gmail.com>
2022-08-13 16:31:02 +0300

    t

--- src/router.js
+++ src/router.js
@@ -23,7 +23,7 @@ const type = {
           })
 
       }
-      else if (endPoint.endsWith(".css") || endPoint.endsWith(".js") ){
+      else if (endPoint.endsWith(".css") || endPoint.endsWith(".js") ||  endPoint.endsWith(".html")){
           const filePath = path.join(__dirname,"..","public",endPoint);
           const extension = path.extname(filePath);
           fs.readFile(filePath,(err,file)=>{
@@ -47,6 +47,51 @@ const type = {
            }
         })
 
+      }
+      else if (endPoint === "/reviews") {
+        const filePath = path.join(__dirname, "reviews.json");
+        fs.readFile(filePath, { encoding: "utf8" }, (error, file) => {
+          if (error) {
+            console.log(error);
+            return;
+          } else {
+            res.writeHead(200, {
+              "Content-Type": "application/json",
+            });
+            res.end(file);
+          }
+        });
+      }
+
+      else if (endPoint === "/create-reviews") {
+        let allTheData = "";
+        req.on("data", (chunkOfData) => {
+          allTheData += chunkOfData;
+        });
+    
+        req.on("end", () => {
+          const SearchParams = new URLSearchParams(allTheData);
+          const filePath = path.join(__dirname, "reviews.json");
+          fs.readFile(filePath, { encoding: "utf8" }, (error, file) => {
+            if (error) {
+              console.log(error);
+              return;
+            } else {
+              res.writeHead(200, {
+                "Content-Type": "application/json",
+              });
+              console.log(SearchParams);
+              let parsed = JSON.parse(file);
+              parsed.push( {"name":SearchParams.get("name") , "review" :SearchParams.get("review") });
+              fs.writeFile(filePath, JSON.stringify(parsed), (err) => {
+                if (err) throw err;
+              });
+            }
+          });
+          res.writeHead(302, { location: "/about.html" });
+          res.end();
+
+        });
       }
       else {
         res.writeHead(404,{"Content-Type" : "text/html"});
