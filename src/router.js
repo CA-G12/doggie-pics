157439a65563aec8afc2bf99f6327c6644561abd
Mai Mohammed <maymohammedabuabed@gmail.com>
2022-08-13 17:07:35 +0300

    add more case

--- src/router.js
+++ src/router.js
@@ -1,8 +1,8 @@
 /* eslint-disable linebreak-style */
 const path = require('path');
 const fs = require('fs');
-const indexHandler = require('./handlers/indexHandler');
-const jsonHandle = require('./handlers/jsonHandler');
+const indexHandler = require('../handlers/indexHandler');
+const jsonHandle = require('../handlers/jsonHandler');
 
 const type = {
   '.js': 'application/javascript',
@@ -14,18 +14,10 @@ const type = {
 const router = (req, res) => {
   const endPoint = req.url;
   if (endPoint === '/') {
-    // const filePath = path.join(__dirname, '..', 'public', 'index.html');
-    // fs.readFile(filePath, (err, file) => {
-    //   if (err) {
-    //     res.writeHead(500, { 'Content-Type': 'text/html' });
-    //     res.end('SERVER ERROR');
-    //   } else {
-    //     res.writeHead(200, { 'Content-Type': 'text/html' });
-    //     res.end(file);
-    //   }
-    // });
+
     indexHandler(res);
-  } else if (endPoint.endsWith('.css') || endPoint.endsWith('.js')) {
+  } 
+  else if (endPoint.endsWith('.css') || endPoint.endsWith('.js')) {
     const filePath = path.join(__dirname, '..', 'public', endPoint);
     const extension = path.extname(filePath);
     fs.readFile(filePath, (err, file) => {
@@ -36,101 +28,59 @@ const router = (req, res) => {
         res.writeHead(200, { 'Content-Type': type[extension] });
         res.end(file);
       }
-<<<<<<< HEAD
-      else if (endPoint.endsWith(".css") || endPoint.endsWith(".js") ||  endPoint.endsWith(".html")){
-          const filePath = path.join(__dirname,"..","public",endPoint);
-          const extension = path.extname(filePath);
-          fs.readFile(filePath,(err,file)=>{
-              if(err){
-                res.writeHead(500,{"Content-Type" : type[extension]});
-                res.end("GO TO HELL");
-              }else{
-                res.writeHead(200,{"Content-Type" : type[extension]});
-                res.end(file);
-              }
-          })
-      }
-      else if(endPoint === "/breeds") {
-          const filePath = path.join(__dirname,"breeds.json");
-          console.log(filePath);
-        fs.readFile(filePath, {'encoding' : "utf-8"} ,(err ,file)=>{
-           if(err){console.log(err) ; return  ; }
-           else{
-            res.writeHead(200,{"Content-Type" : "application/json"});
-            res.end(file);
-           }
-        })
-
-      }
-      else if (endPoint === "/reviews") {
-        const filePath = path.join(__dirname, "reviews.json");
-        fs.readFile(filePath, { encoding: "utf8" }, (error, file) => {
-          if (error) {
-            console.log(error);
-            return;
-          } else {
-            res.writeHead(200, {
-              "Content-Type": "application/json",
-            });
-            res.end(file);
-          }
-        });
-      }
-
-      else if (endPoint === "/create-reviews") {
-        let allTheData = "";
-        req.on("data", (chunkOfData) => {
-          allTheData += chunkOfData;
-        });
-    
-        req.on("end", () => {
-          const SearchParams = new URLSearchParams(allTheData);
-          const filePath = path.join(__dirname, "reviews.json");
-          fs.readFile(filePath, { encoding: "utf8" }, (error, file) => {
-            if (error) {
-              console.log(error);
-              return;
-            } else {
-              res.writeHead(200, {
-                "Content-Type": "application/json",
-              });
-              console.log(SearchParams);
-              let parsed = JSON.parse(file);
-              parsed.push( {"name":SearchParams.get("name") , "review" :SearchParams.get("review") });
-              fs.writeFile(filePath, JSON.stringify(parsed), (err) => {
-                if (err) throw err;
-              });
-            }
-          });
-          res.writeHead(302, { location: "/about.html" });
-          res.end();
-
-        });
-      }
-      else {
-        res.writeHead(404,{"Content-Type" : "text/html"});
-        res.end("Not Found");
-      }
-=======
     });
-  } else if (endPoint === '/breeds') {
-    // const filePath = path.join(__dirname, 'breeds.json');
-    // console.log(filePath);
-    // fs.readFile(filePath, (err, file) => {
-    //   if (err) {
-    //     console.log(err);
-    //     res.writeHead(500, { 'Content-Type': 'application/json' });
-    //     //  return  ;
-    //   } else {
-    //     res.writeHead(200, { 'Content-Type': 'application/json' });
-    //     res.end(file);
-    //   }
-    // });
+  } 
+  else if (endPoint === '/breeds') {
     jsonHandle(res);
-  } else {
+  } 
+  else if (endPoint === "/create-reviews") {
+    let allTheData = "";
+    req.on("data", (chunkOfData) => {
+      allTheData += chunkOfData;
+    });
+
+    req.on("end", () => {
+      const SearchParams = new URLSearchParams(allTheData);
+      const filePath = path.join(__dirname, "reviews.json");
+      fs.readFile(filePath, { encoding: "utf8" }, (error, file) => {
+        if (error) {
+          console.log(error);
+          return;
+        } else {
+          res.writeHead(200, {
+            "Content-Type": "application/json",
+          });
+          console.log(SearchParams);
+          let parsed = JSON.parse(file);
+          parsed.push( {"name":SearchParams.get("name") , "review" :SearchParams.get("review") });
+          fs.writeFile(filePath, JSON.stringify(parsed), (err) => {
+            if (err) throw err;
+          });
+        }
+      });
+      res.writeHead(302, { location: "/about.html" });
+      res.end();
+
+    });
+  }
+  else if (endPoint === "/reviews") {
+    const filePath = path.join(__dirname, "reviews.json");
+    fs.readFile(filePath, { encoding: "utf8" }, (error, file) => {
+      if (error) {
+        console.log(error);
+        return;
+      } else {
+        res.writeHead(200, {
+          "Content-Type": "application/json",
+        });
+        res.end(file);
+      }
+    });
+  }
+
+  else {
     res.writeHead(404, { 'Content-Type': 'text/html' });
     res.end('Not Found');
->>>>>>> 61453bb5a3fa4c06ab201c6525931f2fae0b7a6d
   }
 };
 
